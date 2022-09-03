cdfe0ec712e45cf5e32d74c66d35bb11c35d8bfd
SkPipul <skjabed252@gmail.com>
2022-09-03 11:48:17 +0600

    author info added

--- js/news.js
+++ js/news.js
@@ -40,7 +40,7 @@ const displayNews = totalNews =>{
         
         <div class="row g-0">
                 <div class="col-md-4">
-                  <img src="${news.image_url}" class="w-150 h-100 img-fluid rounded-start" alt="...">
+                  <img src="${news.image_url}" class="img-width h-100 img-fluid rounded-start" alt="...">
                 </div>
                 <div class="col-md-8">
                   <div class="card-body">
@@ -48,17 +48,22 @@ const displayNews = totalNews =>{
                     <p class="card-text p-text">${news.details.slice(0, 150)}...</p>
                     
                   </div>
-                  <div>
+                  <div class="d-flex justify-content-around align-items-center">
+                    <div>
                     <img class="rounded-circle logo" src="${news.author.img}" alt="">
                     <h6>${news.author.name}</h6>
-                    <h5><i class="fa-regular fa-eye"></i>${news.total_view}</h5>
-                    <button></button>
+                    </div>
+                    <h5><i class="bi bi-eye-fill"></i>${news.total_view}</h5>
+                    <button class="btn btn-primary">Details</button>
                   </div>
                 </div>
         </div>
         `;
         newsCard.appendChild(newsDiv)
     })
+
+    
 }
 
 loadAllNews();
+
